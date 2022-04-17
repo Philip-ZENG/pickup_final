b9371c1ea9fcf9eaa043077b3282a0b16f2d3e4a
LucaZhu0219 <120090460@link.cuhk.edu.cn>
2022-04-18 03:01:14 +0800

    personal Information and settings

--- client/src/views/PersonalInfoView.vue
+++ client/src/views/PersonalInfoView.vue
@@ -1,7 +1,83 @@
 <template>
-<div class="personalInfo">
-    <h1>This is the personal info page</h1>
-</div>
+  <div class="personalInfo mt-5">
+    <div class="container-sm p-5 my-5 bg-primary text-white text-center">
+      <h1>Person Profile</h1>
+      <img src="" class="rounded-circle" ><sup v-on:click=""> upload new profile </sup>
+      </div>
+      <div >
+    </div>
+
+    <div class="row">
+        <div class="col-sm-5 p-4">
+            User ID
+        </div>
+
+        <div class="col-sm-7 p-4">
+            This is your user id
+        </div>
+    </div >
+
+    <div class="row">
+        <div class="col-sm-5 p-4">
+            Username
+        </div>
+
+        <div class="col-sm-7 p-4">
+            This is the user name
+        </div>
+    </div >
+
+    <div class="row">
+        <div class="col-sm-5 p-4">
+            Email
+        </div>
+
+        <div class="col-sm-7 p-4">
+            This is the Email Address
+        </div>
+    </div >
+
+    <div class="row">
+        <div class="col-sm-5 p-4">
+            Gender
+        </div>
+
+        <div class="col-sm-7 p-4">
+            
+        <img src="" class="rounded-circle">
+
+        </div>
+    </div >
+
+    <div class="row">
+        <div class="col-sm-5 p-4">
+            Contact Information
+        </div>
+
+        <div class="col-sm-7 p-4">
+            This is the contact Information
+        <img src="" class="rounded-circle">
+
+        </div>
+    </div >
+
+    <div class="row">
+        <div class="col-sm-5 p-4">
+            Bio
+        </div>
+
+        <div class="col-sm-7 p-4">
+
+            <div class="card">
+                <div class="card-body">This is the personal introduction</div>
+            </div>
+        
+        </div>
+    </div >
+
+  </div>
 </template>
 
-<script></script>
+<script>
+// here we need to fetch the data
+</script>
